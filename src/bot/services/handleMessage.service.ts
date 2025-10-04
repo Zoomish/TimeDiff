335c79fa4d8a08309c26a109ae06a21861cfc57e
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-10-04 21:23:52 +0300

    `Updated console.log statements in HandleMessageService to log current date and time instead of message date and time.`

--- src/bot/services/handleMessage.service.ts
+++ src/bot/services/handleMessage.service.ts
@@ -43,10 +43,10 @@ export class HandleMessageService {
             cities = textMain.split(' ')?.join('')
         }
         console.log(
-            new Date(msg.date).toString(),
-            new Date(msg.date).toTimeString(),
-            new Date(msg.date).toUTCString(),
-            new Date(msg.date).toLocaleTimeString()
+            new Date().toString(),
+            new Date().toTimeString(),
+            new Date().toUTCString(),
+            new Date().toLocaleTimeString()
         )
 
         const [city1, city2] = cities.split('->')
