f637fae0a945db94e71a4e63462e51d34c8872f1
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-10-04 16:13:11 +0300

    `Added countries-and-timezones package and modified HandleMessageService to use it`

--- src/bot/services/handleMessage.service.ts
+++ src/bot/services/handleMessage.service.ts
@@ -2,6 +2,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import TelegramBot from 'node-telegram-bot-api'
+const ct = require('countries-and-timezones')
 
 @Injectable()
 export class HandleMessageService {
@@ -26,10 +27,11 @@ export class HandleMessageService {
     private async getTime(textMain: string) {
         const bot: TelegramBot = global.bot
         const msg: TelegramBot.Message = global.msg
+
         if (!textMain) {
             return await bot.sendMessage(
                 msg.chat.id,
-                'Введите данные в формате <code>/time Время Город - Город</code>',
+                'Введите данные в формате <code>/time Время Город -> Город</code>',
                 {
                     parse_mode: 'HTML',
                     reply_markup: {
@@ -38,7 +40,7 @@ export class HandleMessageService {
                                 {
                                     text: 'Скопировать формат',
                                     copy_text: {
-                                        text: '/time Время Город - Город',
+                                        text: '/time Время Город -> Город',
                                     },
                                 },
                             ],
@@ -47,34 +49,67 @@ export class HandleMessageService {
                 }
             )
         }
-        const text = await this.converTime(textMain)
-        return await bot.sendMessage(msg.chat.id, text)
+        const [time, cities] = [
+            textMain.split(' ')?.[0] || null,
+            textMain.split(' ')?.slice(1)?.join('') || '',
+        ]
+        const [city1, city2] = cities.split('->')
+        const timezone = await this.converTime(time, city1, city2)
+        if (timezone === 'error') {
+            return await bot.sendMessage(
+                msg.chat.id,
+                'Не удалось определить таймзону, проверьте названия городов'
+            )
+        }
+        return await bot.sendMessage(
+            msg.chat.id,
+            `Когда в ${city1} ${time}, в ${city2} ${timezone}`
+        )
     }
 
-    private async converTime(text: string) {
+    private async getTimezone(text: string) {
         const geminiToken = this.configService.get('GEMINI_API')
         const genAI = new GoogleGenerativeAI(geminiToken)
         const model = genAI.getGenerativeModel({
             model: 'gemini-2.0-flash',
         })
-        let text1 = 'Произошла ошибка, попробуйте еще раз'
+        let text1 = null
         await model
             .generateContent(
-                'Ты профессиональный переводчик.Ты получаешь данные вида "время Город1 -> Город2".\n' +
-                    'Эта строка значит что ты должен вернуть время в Город2, когда в Город1 время.\n' +
-                    'Ты должен вернуть должен одну строку в формате “Когда в Город1 время, в Город2 …”\n' +
-                    'Если пользователь ввел страну, то в качестве ответа ты должен вернуть время в столице этой страны, но пиши все равно страну в ответе\n' +
-                    'В ответе дай только одну строчку - только время в формате “Когда в Город1 время, в Город2 ...”!!!!\n' +
-                    'Если пользователь ввел что-то другое, или дал недостаточно данных, то в качестве ответа ты должен вернуть “Введите данные в формате /time Время Город - Город”, НО ' +
-                    `Если пользователь ввел только города или страны, дай ответ относительно текущего времени, вот оно в UTC:${new Date().toUTCString().split(' ')[4].split(':').slice(0, 2).join(':')}. Ответ дай не в utc формате, а в местном времени города\n` +
-                    'Не используй никакие разметки в ответе, только текст\n' +
-                    'Вот первая строка: ' +
+                'Ты профессиональный переводчик. Ты получил название города или страны на любом языке, например русском. Переведи его на английский и верни тайм зону.\n' +
+                    'Например, вводит Москва, ты должен ответить Europe/Moscow.\n' +
+                    'Если пользователь ввел страну, верни столицу этой страны. Например, вводит Россия, ты должен ответить Europe/Moscow.\n' +
+                    'Возвращай одно слово-саму зону. Не пиши ничего больше!! \n' +
+                    'Вот первый город: ' +
                     text
             )
             .then((text2) => {
                 text1 = text2.response.text()
             })
-        console.log(text1)
         return text1
+    }
+
+    private async converTime(time: string, city1: string, city2: string) {
+        const city1TimeZone = await this.getTimezone(city1)
+        const city2TimeZone = await this.getTimezone(city2)
+
+        if (city1TimeZone === city2TimeZone) {
+            return time
+        } else if (!city1TimeZone || !city2TimeZone) {
+            return 'error'
+        }
+
+        const timezone1 = ct.getTimezone(city1TimeZone)
+        const date1 = new Date(
+            `August 19, 1975 ${time || new Date().toUTCString().split(' ')[4].split(':').slice(0, 2).join(':')}:00`
+        )
+        date1.setMinutes(date1.getMinutes() + date1.getTimezoneOffset() * -1)
+        date1.setMinutes(date1.getMinutes() + timezone1.dstOffset * -1)
+        return date1.toLocaleTimeString('en-US', {
+            hour12: false,
+            timeZone: city2TimeZone,
+            hour: '2-digit',
+            minute: '2-digit',
+        })
     }
 }
